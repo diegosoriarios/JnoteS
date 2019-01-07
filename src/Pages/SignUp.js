56ca1b059a55f2a0674d635dd6494ee5a51e07d6
diego.rios <diego.rios@CTNAPPLE01YG8WN.local>
2019-01-07 21:28:26 -0200

    CSS

--- src/Pages/SignUp.js
+++ src/Pages/SignUp.js
@@ -1,10 +1,70 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import axios from 'axios';
+import string from '../String';
+import Home from './Home';
 
 class SignUp extends Component {
+    constructor(){
+        super();
+        this.state = {
+            username: '',
+            password: '',
+            passwordConfirmation: '',
+        }
+    }
+
+    register = () => {
+        let date = new Date();
+        axios.post(`${string.URL}/login`, {
+            name: this.state.username,
+            createdAt: date,
+            password: this.state.password,
+          })
+          .then(function (response) {
+            console.log(response);
+          }, () => {
+            return <BrowserRouter>
+                <Route 
+                path='/'
+                render={ props => <Home {...props} />}
+                />
+            </BrowserRouter>
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+    }
+
+    checkPassword = () => {
+        if(this.state.password !== this.state.passwordConfirmation || this.state.password.length === 0){
+            return true;
+        }
+        return false;
+    }
+
     render(){
         return (
-            <div>
-                <h3>Sign Up</h3>
+            <div className="login-box App">
+                <input
+                    type="text"
+                    placeholder="username"
+                    value={this.state.username}
+                    onChange={e => this.setState({username: e.target.value})}
+                /><br />
+                <input
+                    type="password"
+                    placeholder="password"
+                    value={this.state.password}
+                    onChange={e => this.setState({password: e.target.value})}
+                /><br />
+                <input 
+                    type="password"
+                    placeholder="repeat password"
+                    value={this.state.passwordConfirmation}
+                    onChange={e => this.setState({passwordConfirmation: e.target.value})}
+                /><br />
+                <button onClick={() => this.register()} disabled={this.checkPassword()}>Register</button>
             </div>
         );
     }
